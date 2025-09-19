446d088a7905c84b91662d037d155c77d86c9814
Klaussoares <46823372+Klaussoares@users.noreply.github.com>
2025-09-18 21:54:17 -0300

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -20,10 +20,24 @@ MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB
 ALLOWED_EXCEL_EXTENSIONS = ['.xlsx', '.xls']
 ALLOWED_PDF_EXTENSIONS = ['.pdf']
 
-# Coordenadas padrão para cortes das imagens (baseadas no código original)
-BOX_NOME = (766, 359, 1512, 498)
-BOX_MODELO = (1722, 918, 1941, 1025)
-BOX_RESPOSTA = (815, 609, 1807, 824)
+# # Coordenadas padrão para cortes das imagens (baseadas no código original)
+# BOX_NOME = (766, 359, 1512, 498)
+# BOX_MODELO = (1722, 918, 1941, 1025)
+# BOX_RESPOSTA = (815, 609, 1807, 824)
+
+# === Coordenadas para provas novas ===
+COORDS_NOVO = {
+    "BOX_NOME": (766, 359, 1512, 498),
+    "BOX_MODELO": (1722, 918, 1941, 1025),
+    "BOX_RESPOSTA": (815, 609, 1807, 824)
+}
+
+# === Coordenadas para provas antigas ===
+COORDS_ANTIGO = {
+    "BOX_NOME": (658, 233, 1397, 393),
+    "BOX_MODELO": (288, 1021, 693, 1711),
+    "BOX_RESPOSTA": (733, 1048, 1655, 1796)
+}
 
 # Configurações de limpeza automática
 CLEANUP_DELAY_SECONDS = 60  # Tempo para manter arquivos temporários
